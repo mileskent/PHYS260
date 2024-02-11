26ccf6ffe31021da603d8e7e79bcf9f951ff78eb
Miles Kent <mileskent13@gmail.com>
2024-02-11 12:25:06 -0800

    Extra info

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # PHYS 260 Electromagnetism
 
 https://wiki.archlinux.org/title/TeX_Live
+
+In order to build the .tex, run (arch linux) ```chmod +x SETUP.sh && ./SETUP.sh``` for texlive
+
+I am also using VimTex, but it's not required
---
+++ SETUP.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+start="\e["
+green="32m"
+end="\e[0m"
+t="$HOME/.config/nvim"
+pt=$start$green$t
+
+if [ -d $t ]; then
+    echo -e "Moving $pt$end to $pt.bak$end"
+    mv -T $t "$t.bak"
+fi
+
+echo -e "Creating new \"$pt$end\""
+mkdir -p $t
+
+git clone https://github.com/mileskent/Dotfiles "$HOME/.temp"
+echo
+echo "Copying files"
+cp -rv $HOME/.temp/vimtexsetup/* $t/
+
+rm -rf "$HOME/.temp"
+
+yay -S texinfo texlive-basic texlive-bin texlive-binextra texlive-fontsrecommended texlive-latex texlive-latexrecommended texlivemathscience
+
+
+
